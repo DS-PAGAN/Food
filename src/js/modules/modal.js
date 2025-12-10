7c08973d9c477a1b3236c799c1085b1bfdef1260
Denys <denyssamochko@gmail.com>
2025-12-10 21:45:17 +0100

    Make script modules V2 0.0.9

---
+++ src/js/modules/modal.js
@@ -0,0 +1,52 @@
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+  // modal.classList.toggle("show");
+  document.body.style.overflow = "hidden";
+  clearInterval(modalTimerId);
+}
+
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+  // modal.classList.toggle("show");
+  document.body.style.overflow = "";
+}
+
+function modal() {
+  // Modal
+
+  const modalTriger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal");
+
+  modalTriger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal || e.target.getAttribute("data-close") == "") {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
+}
+module.exports = modal;
